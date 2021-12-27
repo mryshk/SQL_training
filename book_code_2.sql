aa216d0e45e2709f267b4ec46ec7566f44bd782e
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2021-12-27 18:55:31 +0900

    Learn replace

---
+++ book_code_2.sql
@@ -0,0 +1,17 @@
+SELECT
+  CONCAT (
+    SUBSTRING(
+      REPLACE(title,'e','3'),
+      1,
+      10
+    ),
+    '...'
+  ) AS 'short title'
+FROM books;
+
+SELECT 
+  CONCAT(
+    REVERSE(author_fname),
+    author_fname
+  )
+FROM books;
