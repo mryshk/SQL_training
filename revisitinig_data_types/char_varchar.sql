f7dd50a867c1f3fa6533f3eaf6945d3cf99b5984
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-07 23:36:50 +0900

    data_type

---
+++ revisitinig_data_types/char_varchar.sql
@@ -0,0 +1,7 @@
+CREATE Table dogs(name CHAR(5), breed VARCHAR(10));
+INSERT INTO
+  dogs(name, breed)
+VALUES("robby", "coogi");
+INSERT INTO
+  dogs(name, breed)
+VALUES("princess jane", "retriver");
