7b9fb1ecd89cca545e71eff247129a9b6bd79ec4
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-09 21:00:21 +0900

    Learn Not equal

---
+++ the_power_of_operators/not_equal.sql
@@ -0,0 +1,24 @@
+SELECT
+  title
+FROM
+  books
+WHERE
+  released_year = 2017;
+SELECT
+  title
+FROM
+  books
+WHERE
+  released_year != 2017;
+SELECT
+  author_lname
+FROM
+  books
+WHERE
+  author_lname = 'Harris';
+SELECT
+  author_lname
+FROM
+  books
+WHERE
+  author_lname != 'Harris';
