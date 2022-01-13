159307f50de3579ecad323ac48c5ae2c55191826
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-13 23:19:54 +0900

    exrecise

--- the_power_of_operators/exercise.sql
+++ the_power_of_operators/exercise.sql
@@ -5,7 +5,6 @@ FROM
   books
 WHERE
   released_year < 1980;
-
 SELECT
   author_lname,
   title
@@ -13,3 +12,12 @@ FROM
   books
 WHERE
   author_lname IN ("Eggers", "Chabon");
+SELECT
+  title,
+  author_lname,
+  released_year
+FROM
+  books
+WHERE
+  author_lname = 'Lahiri'
+  AND released_year >= 2000;
