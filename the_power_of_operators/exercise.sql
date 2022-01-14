7cc843d170465306b5bf68825542d0bf2d639975
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-14 22:31:44 +0900

    Learn between

--- the_power_of_operators/exercise.sql
+++ the_power_of_operators/exercise.sql
@@ -21,3 +21,13 @@ FROM
 WHERE
   author_lname = 'Lahiri'
   AND released_year >= 2000;
+SELECT
+  author_lname,
+  title,
+  released_year
+FROM
+  books
+WHERE
+  released_year BETWEEN '1980' AND '2010'
+ORDER BY
+  released_year;
