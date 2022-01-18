091ff38a10d5a674b49161e0c56f06326c2d5bbd
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-18 23:31:38 +0900

    Learn and do exersice

--- the_power_of_operators/exercise.sql
+++ the_power_of_operators/exercise.sql
@@ -39,6 +39,66 @@ SELECT
 FROM
   books
 WHERE
-  released_year NOT IN (1945,2017, 2001)
+  released_year NOT IN (1945, 2017, 2001)
 ORDER BY
   released_year;
+SELECT
+  title,
+  pages
+FROM
+  books
+WHERE
+  pages BETWEEN 100
+  AND 200;
+SELECT
+  title,
+  author_lname
+FROM
+  books
+WHERE
+  author_lname LIKE "C%"
+  OR author_lname LIKE "S%";
+SELECT
+  title,
+  author_lname
+FROM
+  books
+WHERE
+  SUBSTR(author_lname, 1, 1) = 'C'
+  OR SUBSTR(author_lname, 1, 1) = "S";
+SELECT
+  title,
+  author_lname
+FROM
+  books
+WHERE
+  SUBSTR(author_lname, 1, 1) IN ('S', 'C');
+SELECT
+  title,
+  author_lname,
+  CASE
+    WHEN title LIKE "%stories" THEN "Short Stories"
+    WHEN title = "Just Kids"
+    OR title = "A Heartbreaking Work of Staggering Genius" THEN "Memoir"
+    ELSE "Novel"
+  END AS TYPE
+FROM
+  books;
+SELECT
+  author_lname,
+  author_fname,
+  CASE
+    WHEN COUNT(*) = 1 THEN '1 book'
+    ELSE CONCAT(COUNT(*), " books")
+  END AS COUNT
+FROM
+  books
+GROUP BY
+  author_lname,
+  author_fname;
+SELECT
+  *
+FROM
+  books
+GROUP BY
+  author_lname, author_fname;
