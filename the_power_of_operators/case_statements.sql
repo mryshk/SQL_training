30eff490c3e52cc9db26776c472d4d962734a51b
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-18 23:31:09 +0900

    Learn again

--- the_power_of_operators/case_statements.sql
+++ the_power_of_operators/case_statements.sql
@@ -28,3 +28,30 @@ SELECT
   END AS STOCK
 FROM
   books;
+SELECT
+  title,
+  released_year,
+  CASE
+    WHEN released_year >= 2000 THEN "Modern List"
+    ELSE '20th Century Lit'
+  END AS GENRE
+FROM
+  books
+ORDER BY
+  released_year;
+SELECT
+  title,
+  stock_quantity,
+  CASE
+    WHEN stock_quantity BETWEEN 0
+    AND 50 THEN '*'
+    WHEN stock_quantity BETWEEN 51
+    AND 100 THEN '**'
+    WHEN stock_quantity BETWEEN 101
+    AND 150 THEN '***'
+    ELSE '****'
+  END AS STOCK
+FROM
+  books
+ORDER BY
+  stock_quantity;
