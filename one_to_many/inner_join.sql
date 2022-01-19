fd2f317ceb00e5e483adb7c931c3245abca5d108
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-01-19 18:25:07 +0900

    Learn inner_join

---
+++ one_to_many/inner_join.sql
@@ -0,0 +1,43 @@
+-- INPLICIT INNER JOIN
+SELECT
+  *
+FROM
+  customers,
+  orders
+WHERE
+  customers.id = orders.customer_id;
+-- EXPLICIT INNER JOIN
+SELECT
+  *
+FROM
+  customers
+  JOIN orders ON customers.id = orders.customer_id;
+SELECT
+  first_name,
+  last_name,
+  order_date,
+  amount
+FROM
+  orders
+  JOIN customers ON customers.id = orders.customer_id
+WHERE
+  order_date LIKE "%2016%";
+SELECT
+  *
+FROM
+  customers
+  JOIN orders ON customers.id = orders.customer_id;
+SELECT
+  *
+FROM
+  orders
+  JOIN customers ON orders.customer_id = customers.id;
+
+SELECT
+  order_date,
+  amount,
+  first_name,
+  last_name
+FROM
+  orders
+  JOIN customers ON orders.customer_id = customers.id;
