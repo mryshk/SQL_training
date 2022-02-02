fb809f98ff75b4452e9f666219eab9f459558cc0
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-02 21:23:45 +0900

    learn left_join

---
+++ one_to_many/left_joins.sql
@@ -0,0 +1,26 @@
+SELECT
+  first_name,
+  last_name,
+  SUM(amount) AS total_spent
+FROM
+  customers
+  JOIN orders ON customers.id = orders.customer_id
+GROUP BY
+  orders.customer_id
+ORDER BY
+  amount DESC;
+SELECT
+  *
+FROM
+  customers
+  LEFT JOIN orders ON customers.id = orders.customer_id;
+SELECT
+  first_name,
+  last_name,
+  order_date,
+  IFNULL(amount,0)
+FROM
+  customers
+  LEFT JOIN orders ON customers.id = orders.customer_id
+ORDER BY
+  amount;
