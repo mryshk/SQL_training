19a84af5d478e67dd9f17eb7aabebd75dc0e6769
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-02 21:23:57 +0900

    Learn right_join

---
+++ one_to_many/right_join.sql
@@ -0,0 +1,28 @@
+CREATE TABLE customers(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  email VARCHAR(100)
+);
+CREATE TABLE orders(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  order_date DATE,
+  amount DECIMAL(8, 2),
+  customer_id INT,
+  FOREIGN KEY(customer_id) REFERENCES customers(id) ON DELETE CASCADE
+);
+INSERT INTO
+  customers(first_name, last_name, email)
+VALUES('Boy', 'George', 'george@gmail.com'),
+  ('George', 'Michael', 'gm@gmail.com'),
+  ('David', 'Bowie', 'david@gmail.com'),
+  ('Blue', 'Steele', 'blue@gmail.com'),
+  ('Bette', 'Davis', 'bette@aol.com');
+SELECT
+  IFNULL(first_name, 'MISSING') AS first,
+  IFNULL(last_name, 'USER') AS last,
+  order_date,
+  amount
+FROM
+  customers
+  RIGHT JOIN orders ON customers.id = orders.customer_id;
