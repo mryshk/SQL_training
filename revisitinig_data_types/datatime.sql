595aadb0966672c12018df521f5b8719cee25480
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-16 23:31:44 +0900

    Add practice

--- revisitinig_data_types/datatime.sql
+++ revisitinig_data_types/datatime.sql
@@ -20,3 +20,26 @@ VALUES(
     '00:35:12',
     '1997-01-31 00:35:12'
   );
+CREATE TABLE people2(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  birthdate DATE,
+  birthtime TIME,
+  birthdt DATETIME
+);
+
+INSERT INTO people2(name, birthdate, birthtime, birthdt
+)VALUES(
+  "Bobby",
+  '1992-12-02',
+  '09:02:03',
+  '1997-01-31 00:34:12'
+);
+
+INSERT INTO people2(name, birthdate, birthtime, birthdt
+)VALUES(
+  "Yohoy",
+  "1994-02-02",
+  '12:02:03',
+  '2001-01-12 00:34:12'
+);
