37212985619a4b1f416ac4dfa01f44369191a572
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-16 23:31:38 +0900

    Add practice

--- refering_section/distinct.sql
+++ refering_section/distinct.sql
@@ -4,9 +4,15 @@ SELECT
   DISTINCT CONCAT(author_lname, ' ', author_fname) AS 'distinct full name'
 FROM
   books;
+
+SELECT
+  DISTINCT CONCAT(author_fname, ' ', author_lname) AS "distinct full name"
+FROM
+  books;
 --こちらは結合せずに被りを確認する。
 SELECT
   DISTINCT author_lname,
   author_fname
 FROM
   books;
+
