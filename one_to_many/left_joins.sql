762db302aa6073f51130c324b3038c77c8703818
mryshk <tb.kysy4rsxxxxxx@gmail.com>
2022-02-16 23:31:27 +0900

    Add practice

--- one_to_many/left_joins.sql
+++ one_to_many/left_joins.sql
@@ -15,10 +15,15 @@ FROM
   customers
   LEFT JOIN orders ON customers.id = orders.customer_id;
 SELECT
+  *
+FROM
+  customers
+  LEFT JOIN orders ON customers.id = orders.id;
+SELECT
   first_name,
   last_name,
   order_date,
-  IFNULL(amount,0)
+  IFNULL(amount, 0)
 FROM
   customers
   LEFT JOIN orders ON customers.id = orders.customer_id
